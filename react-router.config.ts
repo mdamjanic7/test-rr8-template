c9c585fb1be3bfe89b6d4dcc4a920f85c7183300
Misha Damjanic <misha.damjanic@shopify.com>
2026-06-26 12:49:32 -0700

    Pin react-router 7.14.0 (npm-strict classic-Hydrogen peer)

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,6 @@ export default {
   appDirectory: "app",
   buildDirectory: "dist",
   ssr: true,
-  subResourceIntegrity: false,
   future: {
     v8_middleware: true,
     v8_splitRouteModules: true,
